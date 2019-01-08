c0c7e0f634e0cd2d2c207d821d4d5f42944a1a6b
Joe Sutton <hungryed6@gmail.com>
2019-01-07 20:18:25 -0800

    add identifiers

--- lib/sungrade_rails_toolkit/office/v0.rb
+++ lib/sungrade_rails_toolkit/office/v0.rb
@@ -13,6 +13,24 @@ module SungradeRailsToolkit
           )
           JSON.parse(res.body).fetch("data").map { |user| new(user) }
         end
+
+        def for_identifiers(*identifiers)
+          res = put(
+            url: File.join(SungradeRailsToolkit.config.api_gateway_base_url, "/api/v0/internal_offices/search"),
+            headers: {"Content-Type" => "application/json"},
+            body: {
+              identifiers: identifiers.compact
+            }.to_json
+          )
+          JSON.parse(res.body).map { |user| new(user) }
+        end
+
+        def for_identifier(identifier)
+          res = get(
+            url: File.join(SungradeRailsToolkit.config.api_gateway_base_url, "/api/v0/internal_offices/by_identifier/#{identifier}"),
+          )
+          new(JSON.parse(res.body))
+        end
       end
 
       def initialize(data)
